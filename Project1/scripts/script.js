5a4a5e6789b22ea4dd862a4d4c3893880e9d18e8
virginie-ronkart <virginie.ronkart@gmail.com>
2016-03-07 16:06:52 +0100

    zoom

--- Project1/scripts/script.js
+++ Project1/scripts/script.js
@@ -1,1 +1,53 @@
-﻿
+﻿var geocoder;
+var map;
+var myCenter = new google.maps.LatLng(51.508742, -0.120850);
+var clickMarker = new google.maps.Marker();
+
+function initialize() {
+    geocoder = new google.maps.Geocoder();
+    var mapProp = {
+        center: myCenter,
+        zoom: 5,
+        disableDefaultUI: true,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    map = new google.maps.Map(document.getElementById("map"), mapProp);
+    map.addListener('click', function (e) {
+        placeMarkerAndPanTo(e.latLng, map);
+    });
+}
+
+function FindAddress() {
+    // Récupération de l'adresse tapée dans le formulaire
+    var adresse = document.getElementById('adresse').value;
+    geocoder.geocode({ 'address': adresse }, function (results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            map.setCenter(results[0].geometry.location);
+            // Récupération des coordonnées GPS du lieu tapé dans le formulaire
+            var strposition = results[0].geometry.location + "";
+            strposition = strposition.replace('(', '');
+            strposition = strposition.replace(')', '');
+            // Affichage des coordonnées dans le <span>
+            //document.getElementById('text_latlng').innerHTML = 'Coordonnées : ' + strposition;
+            // Création du marqueur du lieu (épingle)
+            var marker = new google.maps.Marker({
+                map: map,
+                position: results[0].geometry.location,
+            });
+            map.setZoom(15);
+        } else {
+            alert('Adresse introuvable: ' + status);
+        }
+    });
+}
+
+function placeMarkerAndPanTo(latLng, map) {
+    clickMarker = new google.maps.Marker({
+        position: latLng,
+        map: map
+    });
+    map.panTo(latLng);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
